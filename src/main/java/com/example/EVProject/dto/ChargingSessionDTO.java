b0eea92ef0cd823e799c2de41b6e5b511d80462d
Yashini2002 <yashininethma2002@gmail.com>
2026-04-02 12:03:12 +0530

    session

--- src/main/java/com/example/EVProject/dto/ChargingSessionDTO.java
+++ src/main/java/com/example/EVProject/dto/ChargingSessionDTO.java
@@ -1,31 +1,3 @@
-// package com.example.EVProject.dto;
-
-// import lombok.Getter;
-// import lombok.Setter;
-
-// import java.time.LocalDateTime;
-
-// @Setter
-// @Getter
-// public class ChargingSessionDTO {
-
-//     private Integer sessionId;
-//     private LocalDateTime startTime;
-//     private LocalDateTime endTime;
-//     private Double soc;
-//     private String chargingMode;
-//     private Double totalConsumption;
-//     private Double amount;
-//     private String idDevice;
-
-
-
-
-//     // getters and setters
-
-// }
-
-
 package com.example.EVProject.dto;
 
 import java.time.LocalDateTime;
